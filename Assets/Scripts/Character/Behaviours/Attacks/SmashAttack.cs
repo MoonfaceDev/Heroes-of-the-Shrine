b3bdb0c3dead448f7fd4cb5ea6c5db99a78b2f02
MoonfaceDev <elaicorem1@gmail.com>
2022-09-05 00:10:06 +0300

    Simplify SmashAttack

--- Assets/Scripts/Character/Behaviours/Attacks/SmashAttack.cs
+++ Assets/Scripts/Character/Behaviours/Attacks/SmashAttack.cs
@@ -1,30 +1,6 @@
-public class SmashAttack : SimpleAttack
+public class SmashAttack : NormalAttack
 {
-    public Hitbox hitbox;
     public string spinningSwordsAttackName;
-
-    private SingleHitDetector hitDetector;
-
-    public override void Awake()
-    {
-        base.Awake();
-        hitDetector = new(eventManager, hitbox, (hit) =>
-        {
-            HittableBehaviour hittableBehaviour = hit.GetComponent<HittableBehaviour>();
-            if (hittableBehaviour)
-            {
-                HitCallable(hittableBehaviour);
-            }
-        });
-        onStart += () =>
-        {
-            hitDetector.Start();
-        };
-        onFinish += () =>
-        {
-            hitDetector.Stop();
-        };
-    }
 
     protected override bool CanAttack()
     {
