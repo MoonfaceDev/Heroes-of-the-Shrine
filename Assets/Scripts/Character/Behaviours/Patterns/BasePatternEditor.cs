9f7747443f6bd4b48e75220910cb3f2712b131bf
MoonfaceDev <elaicorem1@gmail.com>
2022-10-11 23:31:03 +0300

    Fix pattern editor script making stuck params

--- Assets/Scripts/Character/Behaviours/Patterns/BasePatternEditor.cs
+++ Assets/Scripts/Character/Behaviours/Patterns/BasePatternEditor.cs
@@ -30,5 +30,7 @@ public class BasePatternEditor : Editor
             EditorGUILayout.PropertyField(maxTime);
             pattern.maxTime = maxTime.floatValue;
         }
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
