880449bed5dc86d6b91cf5de130696246f44d93b
MoonfaceDev <elaicorem1@gmail.com>
2022-11-05 03:34:07 +0200

    Add input buffering

--- Assets/Scripts/Character/Behaviours/Controllers/PlayerController.cs
+++ Assets/Scripts/Character/Behaviours/Controllers/PlayerController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public enum Button
@@ -16,6 +18,13 @@ public class AttackProperty
     public Button button;
 }
 
+public struct BufferedAction
+{
+    public Func<bool> action;
+    public int priority;
+    public float insertionTime;
+}
+
 public class PlayerController : CharacterController
 {
     public AttackProperty[] attacks;
@@ -24,10 +33,20 @@ public class PlayerController : CharacterController
     [HideInInspector]
     public int activeSuitIndex = 0;
 
+    public float bufferingTime;
+    public BaseAttack[] nonBufferedAttacks;
+
+    [Header("Buffered actions priorities")]
+    public int jumpPriority;
+    public int slidePriority;
+    public int attackPriority;
+
     private WalkBehaviour walkBehaviour;
     private JumpBehaviour jumpBehaviour;
     private SlideBehaviour slideBehaviour;
     private DodgeBehaviour dodgeBehaviour;
+
+    private List<BufferedAction> bufferedActions;
 
     public override void Awake()
     {
@@ -36,10 +55,34 @@ public class PlayerController : CharacterController
         jumpBehaviour = GetComponent<JumpBehaviour>();
         slideBehaviour = GetComponent<SlideBehaviour>();
         dodgeBehaviour = GetComponent<DodgeBehaviour>();
+
+        bufferedActions = new();
+    }
+
+    private void Start()
+    {
+        // clear expired buffered actions
+        EventManager.Attach(() => true, () => {
+            bufferedActions = bufferedActions.FindAll(action => Time.time < action.insertionTime + bufferingTime);
+        }, false);
     }
 
     public void Update()
     {
+        // play the buffered action with highest priority
+        if (bufferedActions.Count > 0)
+        {
+            foreach(BufferedAction bufferedAction in bufferedActions.OrderByDescending(action => action.priority))
+            {
+                bool isSuccess = bufferedAction.action();
+                if (isSuccess)
+                {
+                    bufferedActions.Clear();
+                    break;
+                }
+            }
+        }
+
         int horizontal = Direction(Input.GetAxisRaw("Horizontal"));
         int vertical = Direction(Input.GetAxisRaw("Vertical"));
         if (walkBehaviour)
@@ -47,16 +90,40 @@ public class PlayerController : CharacterController
             walkBehaviour.Play(horizontal, vertical);
         }
         //jumping
+        ExecuteJump();
+        //sliding
+        ExecuteSlide();
+        //dodging
+        ExecuteDodge();
+        //attacks
+        ExecuteAttack();
+        //change suits
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            activeSuitIndex = 1 - activeSuitIndex;
+            Animator.runtimeAnimatorController = animatorControllers[activeSuitIndex];
+        }
+    }
+
+    private void ExecuteJump()
+    {
         if (jumpBehaviour && Input.GetButtonDown(Button.Jump.ToString())) //pressed jump
         {
-            jumpBehaviour.Play();
+            ExecuteAction(jumpBehaviour.Play, jumpBehaviour.CanPlay, jumpPriority);
         }
-        //sliding
+    }
+
+    private void ExecuteSlide()
+    {
         if (slideBehaviour && Input.GetButtonDown(Button.Escape.ToString())) //pressed slide
         {
-            slideBehaviour.Play();
+            ExecuteAction(slideBehaviour.Play, slideBehaviour.CanPlay, slidePriority);
         }
-        //dodging
+    }
+
+    private void ExecuteDodge()
+    {
+        int vertical = Direction(Input.GetAxisRaw("Vertical"));
         if (dodgeBehaviour && Input.GetButtonDown(Button.Escape.ToString())) //pressed dodge
         {
             if (vertical != 0)
@@ -64,27 +131,69 @@ public class PlayerController : CharacterController
                 dodgeBehaviour.Play(vertical);
             }
         }
-        //attacks
+    }
+
+    private bool ExecuteAttack(bool isBuffered = false, Button[] bufferedButtons = null)
+    {
         AttackProperty selectedAttack = null;
         foreach (AttackProperty property in attacks)
         {
-            if (Input.GetButtonDown(property.button.ToString()) && property.attack.CanPlay())
+            if (property.attack.CanPlay())
             {
-                selectedAttack = property;
+                if (isBuffered && bufferedButtons != null && bufferedButtons.Contains(property.button))
+                {
+                    selectedAttack = property;
+                }
+                if (!isBuffered && Input.GetButtonDown(property.button.ToString()))
+                {
+                    selectedAttack = property;
+                }
             }
         }
-        if (selectedAttack != null)
+        if (selectedAttack != null && !(isBuffered && nonBufferedAttacks.Contains(selectedAttack.attack)))
         {
             selectedAttack.attack.Play();
             Debug.Log("Started attack " + selectedAttack.attack.AttackName);
-            return;
+            return true;
         }
-        //change suits
-        if (Input.GetKeyDown(KeyCode.Alpha7))
+        else if (!isBuffered)
         {
-            activeSuitIndex = 1 - activeSuitIndex;
-            Animator.runtimeAnimatorController = animatorControllers[activeSuitIndex];
+            Button[] downButtons = GetDownButtons();
+            if (downButtons.Length > 0)
+            {
+                bufferedActions.Add(new BufferedAction { action = () => ExecuteAttack(true, downButtons), priority = attackPriority, insertionTime = Time.time });
+            }
         }
+        return false;
+    }
+
+    private void ExecuteAction(Action play, Func<bool> canPlay, int bufferingPriority)
+    {
+        if (canPlay())
+        {
+            play();
+        }
+        else
+        {
+            bufferedActions.Add(new BufferedAction
+            {
+                action = () => {
+                    if (canPlay())
+                    {
+                        play();
+                        return true;
+                    }
+                    return false;
+                },
+                priority = bufferingPriority,
+                insertionTime = Time.time
+            });
+        }
+    }
+
+    private Button[] GetDownButtons()
+    {
+        return Enum.GetValues(typeof(Button)).Cast<Button>().Where(button => Input.GetButtonDown(button.ToString())).ToArray();
     }
 
     public int Direction(float number)
