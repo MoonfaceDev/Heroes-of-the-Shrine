3a6b43c37af6e22dcb050ee29005818fd48b84d0
MoonfaceDev <elaicorem1@gmail.com>
2022-11-26 01:25:14 +0200

    Fix rotation

--- Assets/Scripts/Objects/General/Hitbox/HitParticles.cs
+++ Assets/Scripts/Objects/General/Hitbox/HitParticles.cs
@@ -12,6 +12,7 @@ public class HitParticles : MonoBehaviour
         var clone = Instantiate(prefab);
         var shape = clone.GetComponent<ParticleSystem>().shape;
         shape.position = MovableObject.ScreenCoordinates(movableObject.TransformToWorld(particlePosition));
+        shape.rotation += movableObject.WorldRotation; 
         clone.GetComponent<Renderer>().sortingOrder = Mathf.Max(
             movableObject.parent.SortingOrder,
             hittable.Character.movableObject.SortingOrder
