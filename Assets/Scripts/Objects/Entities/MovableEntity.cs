5f096a0e5b840131f3556b7570db8f47887388bd
MoonfaceDev <elaicorem1@gmail.com>
2023-02-06 22:39:50 +0200

    Fix collision detection

--- Assets/Scripts/Objects/Entities/MovableEntity.cs
+++ Assets/Scripts/Objects/Entities/MovableEntity.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static MathUtils;
 
 /// <summary>
-/// <see cref="GameEntity"/> with moving capabilities 
+/// <see cref="GameEntity"/> with moving capabilities
 /// </summary>
 [ExecuteInEditMode]
 public class MovableEntity : GameEntity
@@ -14,7 +12,7 @@ public class MovableEntity : GameEntity
     /// Velocity in relative coordinates
     /// </value>
     [ShowDebug] public Vector3 velocity = Vector3.zero;
-    
+
     /// <value>
     /// Acceleration in relative coordinates
     /// </value>
@@ -24,7 +22,7 @@ public class MovableEntity : GameEntity
     /// Invoked when entity tried to move into a barrier
     /// </value>
     public event Action OnStuck;
-    
+
     /// <value>
     /// Invoked when entity landed on the ground (y=0) 
     /// </value>
@@ -33,18 +31,14 @@ public class MovableEntity : GameEntity
     private WalkableGrid walkableGrid;
     private CameraMovement cameraMovement;
 
+    private const float MaxStepSize = 0.01f;
+
     protected override void Awake()
     {
         base.Awake();
 
         walkableGrid = FindObjectOfType<WalkableGrid>();
         if (Camera.main != null) cameraMovement = Camera.main.GetComponent<CameraMovement>();
-
-        OnStuck += () =>
-        {
-            velocity = Vector3.zero + velocity.y * Vector3.up;
-            acceleration = Vector3.zero + acceleration.y * Vector3.up;
-        };
 
         OnLand += () =>
         {
@@ -64,9 +58,7 @@ public class MovableEntity : GameEntity
         // Update position
         var wantedPosition = position + Time.deltaTime * velocity +
                              0.5f * Mathf.Pow(Time.deltaTime, 2) * acceleration;
-        UpdatePosition(position + Vector3.right * (wantedPosition.x - position.x));
-        UpdatePosition(position + Vector3.up * (wantedPosition.y - position.y));
-        UpdatePosition(position + Vector3.forward * (wantedPosition.z - position.z));
+        UpdatePosition(wantedPosition);
 
         // Update scene
         UpdateTransform();
@@ -84,78 +76,37 @@ public class MovableEntity : GameEntity
             return;
         }
 
-        var previousGroundPosition = ToPlane(WorldPosition);
-        var groundPosition = ToPlane(parent ? parent.TransformToWorld(newPosition) : newPosition);
-        var intersections = GetIntersections(previousGroundPosition, groundPosition);
+        var offset = newPosition - position;
+        var direction = offset.normalized;
+        var distance = offset.magnitude;
+        var steps = distance / MaxStepSize + 1;
+        var step = distance / steps * direction;
 
-        if (intersections.Count > 0)
+        for (var i = 0; i < steps; i++)
         {
-            var closest = intersections.Aggregate(groundPosition, (prev, next) =>
-                Vector2.Distance(previousGroundPosition, next) < Vector2.Distance(previousGroundPosition, prev)
-                    ? next
-                    : prev
-            );
-            var newWorldGroundPosition = closest - 0.001f * (groundPosition - previousGroundPosition).normalized;
-            newPosition = ToSpace(parent ? parent.TransformToRelative(newWorldGroundPosition) : newWorldGroundPosition,
-                newPosition.y);
-            OnStuck?.Invoke();
+            AddOffset(step.x * Vector3.right);
+            AddOffset(step.y * Vector3.up);
+            AddOffset(step.z * Vector3.forward);
         }
 
-        if (newPosition.y < 0)
+        if (position.y < 0)
         {
-            newPosition.y = 0;
+            position.y = 0;
             OnLand?.Invoke();
         }
+    }
 
-        position.y = newPosition.y;
+    private void AddOffset(Vector3 offset)
+    {
+        var newPosition = position + offset;
         if (IsValidPosition(newPosition))
         {
             position = newPosition;
         }
-    }
-
-    private List<Vector2> GetIntersections(Vector2 previousGroundPosition, Vector2 groundPosition)
-    {
-        var intersections = GetBarrierIntersections(previousGroundPosition, groundPosition);
-        if (!walkableGrid) return intersections;
-        intersections.AddRange(GetWalkableGridIntersections(previousGroundPosition, groundPosition));
-        if (cameraMovement && tags.Contains(Tag.Player))
+        else
         {
-            intersections.AddRange(GetCameraBorderIntersections(previousGroundPosition, groundPosition));
+            OnStuck?.Invoke();
         }
-
-        return intersections;
-    }
-
-    private static List<Vector2> GetBarrierIntersections(Vector2 previousGroundPosition, Vector2 groundPosition)
-    {
-        var barriers = EntityManager.Instance.GetEntities(Tag.Barrier);
-        List<Vector2> intersections = new();
-        foreach (var barrier in barriers)
-        {
-            var newIntersections = barrier.GetBehaviour<Hitbox>()
-                .GetSegmentIntersections(previousGroundPosition, groundPosition);
-            intersections.AddRange(newIntersections);
-        }
-
-        return intersections;
-    }
-
-    private IEnumerable<Vector2> GetWalkableGridIntersections(Vector2 previousGroundPosition, Vector2 groundPosition)
-    {
-        var gridPosition = ToPlane(walkableGrid.Entity.WorldPosition);
-        var gridSize = ToPlane(walkableGrid.gridWorldSize);
-        return LineRectangleIntersections(previousGroundPosition, groundPosition, gridPosition + gridSize / 2,
-            gridSize);
-    }
-
-    private IEnumerable<Vector2> GetCameraBorderIntersections(Vector2 previousGroundPosition, Vector2 groundPosition)
-    {
-        var border = cameraMovement.border;
-        var gridPosition = ToPlane(walkableGrid.Entity.WorldPosition);
-        var gridSize = ToPlane(walkableGrid.gridWorldSize);
-        return LineRectangleIntersections(previousGroundPosition, groundPosition,
-            new Vector2(border.center.x, gridPosition.y + gridSize.y / 2), new Vector2(border.width, gridSize.y));
     }
 
     private bool IsValidPosition(Vector3 newPosition)
