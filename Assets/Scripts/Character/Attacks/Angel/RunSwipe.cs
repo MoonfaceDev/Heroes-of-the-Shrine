08daffdb26ce1f762742a3da9b42e81994017798
MoonfaceDev <elaicorem1@gmail.com>
2023-02-28 23:33:09 +0200

    Fix RunSwipe

--- Assets/Scripts/Character/Attacks/Angel/RunSwipe.cs
+++ Assets/Scripts/Character/Attacks/Angel/RunSwipe.cs
@@ -1,9 +1,27 @@
 ﻿public class RunSwipe : SimpleAttack
 {
-    protected override MotionSettings Motion => MotionSettings.WalkingDisabled;
+    private WalkBehaviour walkBehaviour;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        walkBehaviour = GetBehaviour<WalkBehaviour>();
+    }
 
     public override bool CanPlay(Command command)
     {
-        return base.CanPlay(command) && IsPlaying<RunBehaviour>();
+        return base.CanPlay(command) && IsPlaying<WalkBehaviour>() && IsPlaying<RunBehaviour>();
+    }
+
+    protected override void DoPlay(Command command)
+    {
+        base.DoPlay(command);
+        MovableEntity.velocity.x = walkBehaviour.speed * Entity.rotation;
+    }
+
+    protected override void DoStop()
+    {
+        base.DoStop();
+        MovableEntity.velocity.x = 0;
     }
 }
