073f2b1df4ebf34e0348eecf7a9269d3dd14388c
MoonfaceDev <elaicorem1@gmail.com>
2023-09-23 04:07:22 +0300

    Improve controller compatibility

---
+++ Assets/Scripts/UI/Panels/GameMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    public UnityEngine.UI.Button defaultSelectedButton;
+
+    public void SetOpened(bool value)
+    {
+        gameObject.SetActive(value);
+        PauseManager.Instance.Paused = value;
+    }
+
+    public bool Opened => gameObject.activeSelf;
+
+    public void Open()
+    {
+        SetOpened(true);
+        defaultSelectedButton.Select();
+    }
+
+    public void Close()
+    {
+        SetOpened(false);
+    }
+}
